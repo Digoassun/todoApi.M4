2cdeb48f58ca6e0943b8e9202bbe399e33fd8e80
Digoassun <digoassun@gmail.com>
2022-07-15 12:25:17 -0300

    Atualização com a aula de quinta

--- src/controller/tarefa-controller.js
+++ src/controller/tarefa-controller.js
@@ -1,3 +1,12 @@
-app.get('/tarefa', (req, res) => {
-    res.send('Rota ativada com GET e recurso tarefa: valores do tarefa devem ser retornados')
-})
+const tarefaController = (app) => {
+    app.get('/tarefa', (req, res) => {
+        res.send('a fazer')
+    })
+
+    app.post('/tarefa', (req, res) => {
+        res.send('Rota POST de tarefa ativada: tarefa adicionado ao bda')
+    })
+
+}
+
+export default tarefaController
--- src/controller/usuario-controller.js
+++ src/controller/usuario-controller.js
@@ -1,3 +1,9 @@
-app.get('/usuario', (req, res) => {
-    res.send('Rota ativada com GET e recurso usuario: valores do usuario devem ser retornados')
-})
+const usuarioController = (app) =>{
+    app.get('/usuario', (req, res) => {
+        res.send('Rota ativada com GET e recurso usuario: valores do usuario devem ser retornados')
+    })
+    app.post('/usuario', (req, res) => {
+        res.send('Rota POST de usuario ativada: usuario adicionado ao bda')
+    })
+}
+export default usuarioController
